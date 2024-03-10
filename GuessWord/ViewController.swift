13982b846400d501b75da641abeecf046166ec23
bekturinho <ismanalievbektur4@gmail.com>
2024-03-09 18:48:14 -0800

    Initial Commit

---
+++ GuessWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessWord
+//
+//  Created by fortune cookie on 3/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
